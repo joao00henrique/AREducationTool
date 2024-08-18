df0d51a00241df2d14778ba45844997ad7b0b79c
joao00henrique <64319174+joao00henrique@users.noreply.github.com>
2024-08-18 12:29:27 +0200

    optimize the naming

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+education_app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@education_app.route('/')
+def display_greeting():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    education_app.run(debug=True)
