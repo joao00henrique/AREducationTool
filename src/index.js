e817c21097b6f1a871d36d2157efc5318b58118e
joao00henrique <64319174+joao00henrique@users.noreply.github.com>
2024-10-14 19:46:15 +0200

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App apiUrl={API_URL} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
