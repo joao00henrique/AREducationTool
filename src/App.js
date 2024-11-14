e8e447796bfdaf14ffcdc24664f6ece181492321
joao00henrique <64319174+joao00henrique@users.noreply.github.com>
2024-11-14 01:15:05 +0100

    function added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppProvider } from './contexts/AppContext'; // Make sure to create AppContext.js
+
 const API_URL = process.env.REACT_APP_API_URL;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App apiUrl={API_URL}/>
+    <AppProvider apiUrl={API_URL}>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// AppContext.js
+import React, { createContext, useContext } from 'react';
+
+const AppContext = createContext();
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
+
+export const AppProvider = ({ apiUrl, children }) => {
+  // Here you can add more application-level state objects or functions
+  
+  const value = { apiUrl };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+// SomeComponent.js
+import React from 'react';
+import { useAppContext } from './contexts/AppContext';
+
+const SomeComponent = () => {
+  const { apiUrl } = useAppContext(); // Now apiUrl can be accessed directly
+
+  return <div>API URL is: {apiUrl}</div>;
+};
+
+export default SomeComponent;
